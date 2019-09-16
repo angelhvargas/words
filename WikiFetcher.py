ca52b5332870293257e04ba6637abbe70ed8efe2
Angel Vargas <angelvargas@outlook.es>
2019-09-16 23:19:09 +0100

    - Renamed test file prefix to make it compatible with pytest. - Fixed typo and added comments. - Added test cases list todo.

--- WikiFetcher.py
+++ WikiFetcher.py
@@ -1,3 +1,5 @@
 
 class WikiFetcher:
+    """class WikiFetcher handle to fetch plain text
+    or limited HTML extracts of the given page"""
     pass
--- tests/unit/test_WikiFetcher.py
+++ tests/unit/test_WikiFetcher.py
@@ -3,7 +3,7 @@ import WikiFetcher
 
 def test_can_instantiate_wiki_fetcher():
     """test WikiFetcher class if does exists"""
-    fetcher = WikiFetcher()
+    fetcher = WikiFetcher
 
 
 
