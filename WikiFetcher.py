984746f59b44fa49c995abada011aa43ac66681a
Angel Vargas <angelvargas@outlook.es>
2019-09-16 22:49:38 +0100

    Test case: class WikiFetcher does exists;
    - created class WikiFetcher

---
+++ WikiFetcher.py
@@ -0,0 +1,3 @@
+
+class WikiFetcher:
+    pass
---
+++ tests/unit/TestWikiFetcher.py
@@ -0,0 +1,9 @@
+import WikiFetcher
+
+
+def test_can_instantiate_wiki_fetcher():
+    """test WikiFetcher class if does exists"""
+    fetcher = WikiFetcher()
+
+
+
